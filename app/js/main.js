3f4a9e35d1a9ff87e6029dd58f2937823a311669
Ilia Green <tim2015web@gmail.com>
2023-08-29 12:32:39 +0300

    complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,23 @@
 // jQuery
 jQuery(document).ready(function () {
-    // Функционал бургера
-    $('#showModal, #closeModal').click(function () {
-        $('#modal').toggleClass('header-modal-menu_show');
-        $('body').toggleClass('body_lock');
-    });
+  // Функционал бургера
+  $('#showModal, #closeModal').click(function () {
+    $('#modal').toggleClass('header-modal-menu_show');
+    $('body').toggleClass('body_lock');
+  });
 
-    // Slick Slider
-    $('.slider').slick({
-        arrows: true,
-        dots: true,
-        slidesToShow: 1,
-        autoplay: true,
-        appendArrows: $('.arrows'), // Внешний блок со стрелками
-        appendDots: $('.dots') // Внешний блок с точками
-    })
+  // Slick Slider
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 1,
+    autoplay: true,
+    appendArrows: $('.arrows'), // Внешний блок со стрелками
+    appendDots: $('.dots') // Внешний блок с точками
+  })
+
+  // Меню бургера
+  $('.header-modal-menu__menu a').click(function () {
+    $('#modal').removeClass('header-modal-menu_show');
+  });
 });
